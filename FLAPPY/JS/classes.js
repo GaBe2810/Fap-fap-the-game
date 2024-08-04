1d6f6e373af0487f65fdb890850101e1ef20bc64
GaBe2810 <gabriel.soares185@etec.sp.gov.br>
2024-08-04 12:38:22 -0300

    acertado

---
+++ FLAPPY/JS/classes.js
@@ -0,0 +1,13 @@
+class obj{
+    constructor (x,y,largura,altura,imagem){
+        this.x = x
+        this.y = y
+        this.largura = largura
+        this.altura = altura
+        this.imagem = imagem
+    }
+    draw(){
+        let img = newImage ()
+        
+    }
+} 
